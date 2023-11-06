cf32fc79f4413bb8f33ddec4a74ff6292e00680e
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2023-11-05 18:46:29 -0700

    error App

---
+++ app/Providers/AdminServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Blade::if('admin', function () {
+            return auth()->check() && auth()->user()->tipo == 'ADMIN';
+        });
+    }
+}
