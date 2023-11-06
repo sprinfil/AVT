df31e41dc2c03d42112f6ed348b6b2ee53c43796
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2023-11-06 00:12:14 -0700

    3

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\Process\Process;
 
