ca0ce5b5643af84ce1cf292bfb8b1328f7191606
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2023-11-05 20:42:32 -0700

    detalle baja persona

--- database/migrations/2023_11_06_032341_add_baja_personas.php
+++ database/migrations/2023_11_06_032341_add_baja_personas.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('personas', function (Blueprint $table) {
-            $table->string('baja')->nullable();
+            $table->string('baja')->nullable()->default(NUll);
         });
     }
 
