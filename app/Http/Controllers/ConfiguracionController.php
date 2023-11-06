bb548cad345919614839fa04d69f4d90ed788181
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2023-11-06 00:11:35 -0700

    3

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
 
 class ConfiguracionController extends Controller
 {
@@ -67,13 +68,37 @@ class ConfiguracionController extends Controller
 
     public function actualizar_sistema(){
         //estos caracteres: 2>&1 sirven para mostrar el resultado del comando 
-
-        $actualizar_sistema = exec('git pull origin main 2>&1');
+        /*
+                $actualizar_sistema = exec('git pull origin main 2>&1');
 
         Artisan::call('migrate');
 
         $result = $actualizar_sistema ." ". Artisan::output();
         echo $result . "Refresque la pagina para aplicar los cambios";
+        */
+            // Define el comando Git que deseas ejecutar
+            $gitCommand = 'git pull origin main';
+
+            // Crea una nueva instancia de Symfony Process
+            $process = new Process(explode(' ', $gitCommand));
+
+            // Establece la ruta de trabajo (directorio) donde se ejecutará el comando Git
+            $process->setWorkingDirectory('/var/www/AVT');
+
+            // Ejecuta el comando Git
+            $process->run();
+
+            // Obtiene la salida del comando Git
+            $output = $process->getOutput();
+
+            // Comprueba si la ejecución del comando fue exitosa
+            if ($process->isSuccessful()) {
+                // El comando Git se ejecutó con éxito
+                echo "Comando Git ejecutado con éxito. Salida: $output";
+            } else {
+                // Hubo un error al ejecutar el comando Git
+                echo "Error al ejecutar el comando Git. Salida: $output";
+            }
     }
 
     public function generar_copia_seguridad(){
