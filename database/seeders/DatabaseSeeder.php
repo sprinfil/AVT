2776d09affdb9ae40109f37198d70f464c49ccd5
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2023-12-15 09:30:36 -0700

    error en el seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create();
+        //\App\Models\User::factory()->create();
         
         if (!DB::table('seeders_log')->where('seeder_name', 'EntidadesSeeder')->exists()) {
             // Ejecuta el seeder
